bbfb9318104b248fea2d53467934bedb293bda7f
Jade Kneen <48185660+JadeKneen@users.noreply.github.com>
2021-10-02 20:25:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import HackInHere from './HackInHere';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Happy HacktoberFest 2021!</h1>
+        <br/>
+        <h3>Hi there - my name is Jade and I am the maintainer of this project.</h3>
+        <p>Our task is to build an application which can be used during squad retros.</p>
+        <p>If you haven't taken part in a retro before, then it is somewhere you can discuss how you felt about your last project or sprint.</p>
+        <p>With a lot of companies going remote we need an online tool to use for this.</p>
+        <p>So let's build one.</p>
+        <p>You have the freedom to build whatever you want to add to this project. It can be big or small. What do you think it needs?</p>
+      <h3>Where do I make changes?</h3>
+        <p>Make your changes in the HackInHere.js file <b>not</b> App.js</p>
+        <p>These changes will appear in the box below:</p>
+        </header>
+      <HackInHere/>
     </div>
   );
 }
---
+++ src/HackInHere.js
@@ -0,0 +1,13 @@
+import './HackInHere.css'
+
+function HackInHere() {
+    return (
+      <div className="HackInHere">
+        <div className="container">
+          {/* Add your changes in here */}
+        </div>
+      </div>
+    );
+  }
+  
+  export default HackInHere;
